428b2f7044a8b84d145f6ae758f6a1b1f5f7ab27
brunonelson <nelson.bruno@f1006-23-34167.bmcc.cuny.edu>
2019-09-05 19:00:46 -0400

    Update sketch.js

--- story/sketch.js
+++ story/sketch.js
@@ -1,15 +1,33 @@
-var x = 100;
-var y = 200;
-var s = 300;
+/*
+story
+9.5.2019
+nelson bruno
+mmp310
+*/
 
-var greeting = '"hello!"';
+// character variables
+var x = 200;
+var y = 100;
+var faceSize = 200;
+var eyeSize = 20;
+var eyeOffset = 50;
+var c = 'purple'; // color
+var greeting = '"Hi!"';
 
 function setup() {
-  createCanvas(windowWidth,600);
-  background('black');
-    
-    ellipse( x, y, s);
-    
-    textsize(40);
-    text(greeting, x, y);
+	createCanvas(windowWidth, 600);
+	background('black');
+	
+	fill(c);
+	ellipse(x, y, faceSize); // face
+	fill('black');
+	stroke('white');
+	strokeWeight(4);
+	rect(x - eyeOffset, y - eyeOffset, eyeSize, eyeSize); // left eye
+	rect(x + eyeOffset, y - eyeOffset, eyeSize, eyeSize); // right eye
+	
+	textSize(40);
+	fill('white');
+	noStroke();
+	text(greeting, x + 100, y - 50);
 }
