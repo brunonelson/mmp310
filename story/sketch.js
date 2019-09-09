c7c94b3ad09650f24f62cd6e9a491525af151e49
brunonelson <nelson.bruno@f1006-12-34123.bmcc.cuny.edu>
2019-09-09 19:04:48 -0400

    Update sketch.js

--- story/sketch.js
+++ story/sketch.js
@@ -6,28 +6,34 @@ mmp310
 */
 
 // character variables
+//character#1
 var x = 200;
-var y = 100;
+var y = 200;
 var faceSize = 200;
-var eyeSize = 20;
-var eyeOffset = 50;
-var c = 'purple'; // color
-var greeting = '"Hi!"';
+var eyeSize = 40;
+var eyeOffset = 40;
+var c = 'orangered'; // color
+var greeting = '"Hello my dude!"';
 
 function setup() {
 	createCanvas(windowWidth, 600);
 	background('black');
-	
+    
+	//character#1
 	fill(c);
 	ellipse(x, y, faceSize); // face
-	fill('black');
+	fill('orange');
 	stroke('white');
 	strokeWeight(4);
-	rect(x - eyeOffset, y - eyeOffset, eyeSize, eyeSize); // left eye
-	rect(x + eyeOffset, y - eyeOffset, eyeSize, eyeSize); // right eye
+	ellipse(x - eyeOffset, y - eyeOffset, eyeSize, eyeSize); // left eye
+	ellipse(x + eyeOffset, y - eyeOffset, eyeSize, eyeSize); // right eye
 	
 	textSize(40);
 	fill('white');
 	noStroke();
 	text(greeting, x + 100, y - 50);
+    
+    //chracter#2
+    fill(c);
+    rect(x, y, faceSize);//face
 }
