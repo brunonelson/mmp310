1540589bfc83fddae5560561dfdbdba228d68612
brunonelson <47126837+brunonelson@users.noreply.github.com>
2019-10-23 15:49:33 -0400

    change

--- interface/sketch.js
+++ interface/sketch.js
@@ -3,7 +3,9 @@
 	user interface
 	10.16.2019
 */
-
+var r = 221;
+var g = 160;
+var b = 221;
 var num = 50;
 var nameInput;
 
@@ -11,7 +13,7 @@ function setup() {
 	createCanvas(windowWidth, 600);
 	pattern();
 	
-	var button = createButton("Generate Pattern");
+	var button = createButton("change back");
 	button.mouseClicked(pattern);
 	
 	nameInput = createInput("Name the file");
@@ -19,10 +21,19 @@ function setup() {
 	saveButton.mouseClicked(saveImage);
 	
 	createP("Change pattern");
-	var numSlider = createSlider(2, 100, num);
+	var numSlider = createSlider(1, 100, num);
 	numSlider.input(getNum);
+    
+    createP("Change background color");
+	var redSlider = createSlider(100, 255, r);
+	//redSlider.input(updateRed);
 	
 }
+
+//function updateRed() {
+	//r = this.value();
+	//backgroundcolor();
+//}
 
 function getNum() {
 	num = this.value();
@@ -44,6 +55,9 @@ function pattern() {
 		ellipse(random(width), random(height), random(50, 100));
         fill("red");
         rect( random(width) , random( height) , random(100) , random(50));
+        fill("blue");
+        //quad(random(width), random(height), random(50), random(20), random(69), random(63),random(30), random(76));
+        
 	}
     }
 
